6fdfe18f824cd709510ecb7d397f5436d582e689
Gantulga <gangstarmn@gmail.com>
2016-07-25 12:40:37 +0900

    version 0.1.3

--- GSMaterialTextField/GSMaterialTextField.m
+++ GSMaterialTextField/GSMaterialTextField.m
@@ -26,15 +26,18 @@ static NSString *bundleName = @"GSTextField";
 #endif
 
 #pragma mark - Get Methods
+
 - (UIColor *)errorColor {
     if (!_errorColor) {
         _errorColor = [UIColor redColor];
     }
     return _errorColor;
 }
+
 -(UIView *)seperatorView {
     if (!_seperatorView) {
-        _seperatorView = [[UIView alloc] initWithFrame:CGRectMake(5, 45, self.bounds.size.width-10, 2)];
+        _seperatorView = [[UIView alloc] initWithFrame:CGRectMake(5, self.bounds.size.height-11, self.bounds.size.width-10, 2)];
+        _seperatorView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
         _seperatorView.backgroundColor = self.normalColor;
     }
     return _seperatorView;
